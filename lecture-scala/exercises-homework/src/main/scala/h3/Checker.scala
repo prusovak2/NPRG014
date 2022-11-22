79db96995f87e12b5511bfbaee66fb0b2a2ddbb1
prusovak <katerina.prusova@quanti.cz>
2022-11-22 23:21:35 +0100

    scala hw 3

--- lecture-scala/exercises-homework/src/main/scala/h3/Checker.scala
+++ lecture-scala/exercises-homework/src/main/scala/h3/Checker.scala
@@ -1,23 +1,26 @@
-/* Uncomment this to finalize the code
+// Uncomment this to finalize the code
 
 package h3
 
 import scala.collection.mutable.ListBuffer
+import scala.compiletime.ops.boolean
 
 abstract class Event
 
 case class Command(cmdName: String) extends Event
 
-case class Succeed(cmdName: String) extends Event
+case class Succeed(cmdName: String) extends Event // command with name cmdName succeeded, two commands with the same name won't exists at the same time
 
 case class Fail(cmdName: String) extends Event
 
 class Property(val name: String, val func: () => Boolean)
 
+// base class for classes checking the rules
 class Monitor[T]:
   val properties = ListBuffer.empty[Property]
+  var is_satisfied = true
 
-  // def property /* Add declaration here */
+  def property(propName: String)(formula: => Boolean) = /* Add declaration here */
     properties += Property(propName, () => formula)
 
   var eventsToBeProcessed = List[T]()
@@ -30,8 +33,16 @@ class Monitor[T]:
 
       println("Property \"" + prop.name + "\" ... " + (if (result) "OK" else "FAILED"))
 
+  //def simplifyUsing(func: PartialFunction[Expr, Expr]) =
+	//	if func.isDefinedAt(this) then func(this) else this
 
   def require(func: PartialFunction[T, Boolean]): Boolean =
+    val definedEvents = eventsToBeProcessed.dropWhile(event => !func.isDefinedAt(event))
+    if(!definedEvents.isEmpty) 
+      eventsToBeProcessed = definedEvents.tail 
+      func(definedEvents.head)
+    else true
+
   /* Add body here
    *
    * to know whether a partial function is defined for a given event,
@@ -40,13 +51,15 @@ class Monitor[T]:
 
 
 class MyMonitor extends Monitor[Event] :
+
+  // writing dsl here
   property("The first command should succeed or fail before it is received again") {
     require {
-      case Command(c) =>
-        require {
+      case Command(c) => // if I see a  command c
+        require { // at some time in the future I wanna see the command c (`c` so that same c is used -> new var c is not created when pattern matching) to succeed or yo fail
           case Succeed(`c`) => true
           case Fail(`c`) => true
-          case Command(`c`) => false
+          case Command(`c`) => false // I do not wanna see the same command again - command should complete before it is issued again
         }
     }
   }
@@ -72,6 +85,13 @@ class MyMonitor extends Monitor[Event] :
     /* Add a property definition here which requires that the first command does not fail.
      * It should yield OK with the events listed in the main method.
      */
+    require {
+      case Command(c) => // if I see a  command c
+        require { 
+          case Succeed(`c`) => true
+          case Fail(`c`) => false
+        }
+    }
   }
 
 
@@ -88,4 +108,25 @@ object Checker:
     val monitor = new MyMonitor
     monitor.check(events)
 
- */
+    /*val events2 = List(
+      Command("take_picture"),
+      Command("get_position"),
+      Command("take_picture"),
+      Succeed("get_position"),
+      Fail("take_picture")
+    )
+    monitor.check(events2)
+
+    val events3 = List(
+      Command("take_picture"),
+      Command("get_position"),
+      Fail("get_position")
+    )
+
+    monitor.check(events3)
+
+    val events4 = List(
+      Command("take_picture"),
+      Command("get_position"),
+      Fail("take_picture")
+    )*/
